5f1306343be60811cbc57dd484f5019e274885e2
Liam <github@liamjones.org>
2025-01-08 19:54:32 +0800

    Rebuilding in python start

---
+++ app.py
@@ -0,0 +1,69 @@
+import seaborn as sns
+
+# Import data from shared.py
+from shared import df
+import pandas as pd
+from pathlib import Path
+from shiny.express import input, render, ui
+import shinyswatch
+
+
+#def abxdata():
+ #   infile = Path(__file__).parent / "anitbioticsdata.csv"
+  #  return pd.read_csv(infile)
+
+abxdata = pd.read_csv(Path(__file__).parent / "antibiotics_data.csv") #imports csv
+
+
+
+ui.page_opts(title="PenicillinX!",
+             window_title="PynicillinX",
+             fillable=True,
+             theme=shinyswatch.theme.superhero,
+             style="text-align: center;"
+             )
+
+with ui.tags.head(): # Edits the title to centre it
+    ui.tags.style("""
+        .navbar-brand {
+            text-align: center;
+            flex-grow: 1;
+        }
+        .navbar-header {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+        }
+    """)
+
+
+
+with (ui.sidebar(open="always")):
+    ui.input_selectize(
+        id="abx1",
+        label="Select Antibiotic 1:",
+        selected="",
+        choices=[""] + abxdata["Antibiotic"].tolist(),
+        multiple=False,
+        options={"Placeholder": "CHECK12", "allowEmptyOption": True})
+
+    ui.input_selectize(
+        id="abx2",
+        label="Select Antibiotic 2:",
+        selected="Select Antibiotic",
+        choices=["Select Antibiotic"] + abxdata["Antibiotic"].tolist(),
+        options={"Placeholder": "Select Please", "allowEmptyOption": False})
+
+    ui.input_selectize(
+        id="abx3",
+        label="Select Antibiotic 3:",
+        selected="Select Antibiotic",
+        choices=["Select Antibiotic"] + abxdata["Antibiotic"].tolist(),
+        options={"Placeholder": "Select Please", "allowEmptyOption": False})
+
+
+
+
+
+
+
